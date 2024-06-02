a6d589d21bd6bc29e178dbe13cf54a8fafef30b8
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2024-06-02 15:07:01 +0200

    add mason and LSP, install multiple lsp server

--- lua/myconfig/vim-plug.lua
+++ lua/myconfig/vim-plug.lua
@@ -10,6 +10,16 @@ Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate'})
 Plug('mbbill/undotree')
 Plug('tpope/vim-fugitive')
 
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+
+Plug('neovim/nvim-lspconfig')
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('L3MON4D3/LuaSnip')
+
+Plug('VonHeikemen/lsp-zero.nvim', { ['branch'] = 'v3.x'})
+
 vim.call('plug#end')
 
 vim.cmd[[colorscheme tokyonight]]
