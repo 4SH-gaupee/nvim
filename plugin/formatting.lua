61eac8ee53fd9cddcfceb6548a592ebf46f48a03
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2024-07-16 11:48:27 +0200

    add bunch of plugins

--- plugin/formatting.lua
+++ plugin/formatting.lua
@@ -1,25 +1,24 @@
-    local conform = require("conform")
-    require("conform").setup({
-    	formatters_by_ft = {
-    		xml = { "xmlformat" },
-    		json = { "prettier" },
-    		hcl = { "hclfmt" },
-    		yaml = { "yamlfmt" },
-    		ansible = { "yamlfmt" },
-    		markdown = { "prettier" },
-    		lua = { "stylua" },
-    		python = { "black" },
-    	},
-    	format_on_save = {
-    		lsp_fallback = true,
-    		async = false,
-    		timeout_ms = 1000,
-    	},
-    	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-    		conform.format({
-    			lsp_fallback = true,
-    			async = false,
-    			timeout_ms = 500,
-    		})
-    	end, { desc = "Format file or range (in visual mode)" }),
-    })
+local conform = require("conform")
+require("conform").setup({
+	formatters_by_ft = {
+		xml = { "xmlformat" },
+		json = { "prettier" },
+		hcl = { "hclfmt" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		lua = { "stylua" },
+		python = { "black" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	},
+	vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 500,
+		})
+	end, { desc = "Format file or range (in visual mode)" }),
+})
