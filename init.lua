e770f14bbce1e16a791b682f08c7ab88cf2bc646
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-01-14 17:22:00 +0100

    chore: enable yamlls

--- init.lua
+++ init.lua
@@ -641,6 +641,28 @@ require("lazy").setup({
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
 				-- ts_ls = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							format = {
+								enable = true,
+							},
+							schemas = {
+								kubernetes = "*.yaml",
+								["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+								["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+								["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+								["http://json.schemastore.org/kustomization"] = "**/kustomization.{yml,yaml}",
+								["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+								["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+								["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+								["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+								["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+								["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+							},
+						},
+					},
+				},
 				--
 
 				lua_ls = {
