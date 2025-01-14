9e284970908806607d5e110047dd4c254108060f
Gwendal AUPEE <gwendal.aupee@4sh.fr>
2025-01-14 17:32:01 +0100

    fix: add kubernetes autocompletion

--- lua/custom/filetype.lua
+++ lua/custom/filetype.lua
@@ -1,3 +1,9 @@
+vim.opt_local.cursorcolumn = true -- Highlight the current column
+vim.opt_local.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.opt_local.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt_local.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.opt_local.expandtab = true -- Expand tab to 2 spaces
+
 vim.filetype.add({
 	pattern = {
 		-- ansible playbook
@@ -10,21 +16,21 @@ vim.filetype.add({
 		[".*/.j2"] = "jinja",
 	},
 })
-local lint_augroup = vim.api.nvim_create_augroup('setIndent', { clear = true })
-vim.api.nvim_create_autocmd('Filetype', {
-    group = 'setIndent',
-    pattern = {
-        'css',
-        'html',
-        'javascript',
-        'lua',
-        'markdown',
-        'md',
-        'typescript',
-        'scss',
-        'xml',
-        'xhtml',
-        'yaml'
-    },
-    command = 'setlocal shiftwidth=2 tabstop=2'
+local lint_augroup = vim.api.nvim_create_augroup("setIndent", { clear = true })
+vim.api.nvim_create_autocmd("Filetype", {
+	group = "setIndent",
+	pattern = {
+		"css",
+		"html",
+		"javascript",
+		"lua",
+		"markdown",
+		"md",
+		"typescript",
+		"scss",
+		"xml",
+		"xhtml",
+		"yaml",
+	},
+	command = "setlocal shiftwidth=2 tabstop=2",
 })
